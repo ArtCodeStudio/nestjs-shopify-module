20d203d242ec115b55700cdc5d32152a24671d6f
JumpLink <pascal@jumplink.eu>
2018-12-28 16:57:26 +0100

    [Webhooks] Get host from config

--- src/webhooks/webhooks.service.ts
+++ src/webhooks/webhooks.service.ts
@@ -2,13 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { Webhooks } from 'shopify-prime';
 import { Webhook } from 'shopify-prime/models';
 import { IShopifyConnect } from '../auth/interfaces/connect';
+import { ShopifyModuleOptions } from '../interfaces/shopify-module-options';
 
 @Injectable()
 export class WebhooksService {
+  constructor(
+    private shopifyModuleOptions: ShopifyModuleOptions,
+  ) {}
   public create(shopifyConnect: IShopifyConnect, topic: string) {
     const webhooks = new Webhooks(shopifyConnect.myshopify_domain, shopifyConnect.accessToken);
     return webhooks.create({
-      address: 'https://gobd-next.artandcode.studio/webhooks/'+topic,
+      address: `https://${this.shopifyModuleOptions.appHost}/webhooks/${topic}`,
       topic
     });
   }
