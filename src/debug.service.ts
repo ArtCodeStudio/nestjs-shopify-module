b3bc4ce62b6986032e4b8c7e98f6f0521bcaaa32
Moritz Raguschat <moritz@jumplink.de>
2019-02-14 19:27:22 +0100

    rewrite debug service

--- src/debug.service.ts
+++ src/debug.service.ts
@@ -1,12 +1,65 @@
-import { LoggerService, Logger } from '@nestjs/common';
-import Debug from 'debug';
+import { Logger } from '@nestjs/common';
 
-export class DebugService extends Logger implements LoggerService {
+export class DebugService {
 
-  public debug: Debug.IDebugger;
-
+  private context: string;
   constructor(name: string) {
-    super(name);
-    this.debug = Debug(`${name}`);
+    this.context = name;
+  }
+
+  public log(...args: any[]) {
+    Logger.log(
+      args
+      .map((arg) => {
+        if (typeof arg === 'object') {
+          return JSON.stringify(arg, null, 2);
+        }
+        return arg;
+      })
+      .join('\n'),
+      this.context,
+    );
+  }
+
+  public debug(...args: any[]) {
+    this.log(...args);
+  }
+
+  public warn(...args: any[]) {
+    Logger.warn(
+      args
+      .map((arg) => {
+        if (typeof arg === 'object') {
+          return JSON.stringify(arg, null, 2);
+        }
+        return arg;
+      })
+      .join('\n'),
+      this.context,
+    );
+  }
+
+  public error(...args: any[]) {
+    const msgs = [];
+    const traces = [];
+    args.forEach((arg) => {
+      if (typeof arg === 'object') {
+        if (arg instanceof Error) {
+          msgs.push(arg.message);
+          traces.push(arg.stack);
+        } else {
+          msgs.push(JSON.stringify(arg, null, 2));
+        }
+      } else {
+        msgs.push(arg);
+      }
+    });
+    let trace = '';
+    if (traces.length === 1) {
+      trace = traces[0];
+    } else if (traces.length > 1) {
+      trace = traces.map((t, i) => `[${i+1}] ${t}`).join('\n');
+    }
+    Logger.error(msgs.join('\n'), trace, this.context);
   }
 }
