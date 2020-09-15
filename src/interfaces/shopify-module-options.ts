6f2083af8b3b07e7600841f6968a3e44709f53c2
art-and-code-studio <pascal@artandcode.studio>
2020-09-15 19:26:52 +0200

    Seperate config interfaces

--- src/interfaces/shopify-module-options.ts
+++ src/interfaces/shopify-module-options.ts
@@ -3,64 +3,76 @@ import { Topic } from '../interfaces/webhook';
 import * as redisStore from 'cache-manager-ioredis';
 import { Redis } from 'ioredis';
 
+export interface ConfigApp {
+  protocol: 'https' | 'http';
+  host: string;
+  port: number;
+  debug: boolean;
+  environment: 'production' | 'development' | 'test';
+}
+
+export interface ConfigShopify {
+  /** client id / Api key */
+  clientID: string;
+  /**  shared secret / client Secret / API secret key */
+  clientSecret: string;
+  /** callback url / redirect url */
+  callbackURL: string;
+  /** callback url used in shopify iframe */
+  iframeCallbackURL: string;
+  // successRedirectURL?: string;
+  // failureRedirectURL?: string;
+  scope: string[];
+  webhooks: {
+    autoSubscribe: Topic[];
+  };
+}
+
+export interface ConfigCharges {
+  plans: IPlan[];
+  frontend_return_url: string;
+}
+
+export interface ConfigCache {
+  store: 'memory' | typeof redisStore;
+  ttl: number;
+  max: number;
+  [Key: string]: any;
+}
+
+export interface ConfigRedis {
+  host?: string;
+  auth_pass?: string;
+  username?: string;
+  port?: number;
+  url?: string;
+}
+
+export interface ConfigMongoDB {
+  host?: string;
+  port?: number;
+  username?: string;
+  password?: string;
+  database?: string;
+  /** Optional MongoDB connection string */
+  url?: string;
+}
 export interface ShopifyModuleOptions {
 
-  app: {
-    protocol: string;
-    host: string;
-    port: number;
-    debug: boolean;
-    environment: 'production' | 'development' | 'test';
-  };
+  app: ConfigApp;
 
-  shopify: {
-    /** client id / Api key */
-    clientID: string;
-    /**  shared secret / client Secret / API secret key */
-    clientSecret: string;
-    /** callback url / redirect url */
-    callbackURL: string;
-    /** callback url used in shopify iframe */
-    iframeCallbackURL: string;
-    // successRedirectURL?: string;
-    // failureRedirectURL?: string;
-    scope: string[];
-    webhooks: {
-      autoSubscribe: Topic[];
-    };
-  };
+  shopify: ConfigShopify;
 
-  charges: {
-    plans: IPlan[];
-    frontend_return_url: string;
-  };
+  charges: ConfigCharges;
 
   /**
    * Cache manager options
    * @see https://github.com/BryanDonovan/node-cache-manager
    */
-  cache: {
-    store: 'memory' | typeof redisStore;
-    ttl: number;
-    max: number;
-    [Key: string]: any;
-  }
+  cache: ConfigCache;
 
-  redis?: {
-    host: string;
-    auth_pass: string;
-    username?: string;
-    port: number;
-  };
+  redis?: ConfigRedis;
 
-  mongodb: {
-    host: string;
-    port: number;
-    username: string;
-    password: string;
-    database: string;
-    /** Optional MongoDB connection string */
-    url?: string;
-  };
+  mongodb: ConfigMongoDB;
 
 }
