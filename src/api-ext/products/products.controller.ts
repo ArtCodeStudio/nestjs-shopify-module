d13f0ba1345361a5e764bdc9c14c5c8ac090cf7c
trollkotze <moritz.raguschat@web.de>
2020-12-30 02:53:53 +0000

    [Cache] revert back to old hacky method (also for ProductExt) as long as we use JSONP and not CORS

--- src/api-ext/products/products.controller.ts
+++ src/api-ext/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, UseGuards, UseInterceptors, CacheTTL, Req, Res, Get, HttpStatus, Query } from '@nestjs/common';
+import { Controller, UseGuards, Inject, Req, Res, Get, HttpStatus, Query } from '@nestjs/common';
 import { Response } from 'express';
 import { Roles } from '../../guards/roles.decorator';
 import { ExtProductsService } from './products.service';
@@ -7,15 +7,30 @@ import { ShopifyApiGuard } from '../../guards/shopify-api.guard';
 import { IUserRequest } from '../../interfaces/user-request';
 import { RolesGuard } from '../../guards/roles.guard';
 import { SortKey } from './products.service';
-import { ApiCacheInterceptor } from '../../api/api-cache.interceptor';
+
+// import { ApiCacheInterceptor } from '../../api/api-cache.interceptor';
+import { SHOPIFY_MODULE_OPTIONS } from '../../shopify.constants';
+import { ShopifyModuleOptions } from '../../interfaces/shopify-module-options';
+
+// TODO: As long as we use JSONP and not CORS, we can't use interceptors and need this workaround:
+import * as cacheManager from 'cache-manager';
+import { Cache } from '../../api/interfaces/api-cache';
 
 @Controller('shopify/api-ext/products')
-@UseInterceptors(ApiCacheInterceptor)
+//@UseInterceptors(ApiCacheInterceptor)
 export class ExtProductsController {
   constructor(
     protected readonly extProductsService: ExtProductsService,
-  ) {}
+    @Inject(SHOPIFY_MODULE_OPTIONS) private readonly shopifyModuleOptions: ShopifyModuleOptions,
+  ) {
+    if (!this.shopifyModuleOptions.cache && this.shopifyModuleOptions.cache.store) {
+      throw new Error('You need a cache');
+    }
+    this.cache = cacheManager.caching(this.shopifyModuleOptions.cache) as Cache;
+  }
 
+  cache: Cache;
+  
   logger = new DebugService(`shopify:${this.constructor.name}`);
 
   /**
@@ -26,7 +41,7 @@ export class ExtProductsController {
    */
   @UseGuards(ShopifyApiGuard, RolesGuard)
   @Roles() // Also allowed from shop frontend
-  @CacheTTL(300)
+  // @CacheTTL(300)
   @Get('scheduled')
   async listScheduled(
     @Req() req: IUserRequest,
@@ -35,17 +50,30 @@ export class ExtProductsController {
     @Query("limit") limit = 50,
     @Query("after") after,
     @Query("sortKey") sortKey = "ID",
-    @Query("reverse") reverse = false
-  ) {console.log( SortKey[sortKey as keyof typeof SortKey])
+    @Query("reverse") reverse = false,
+    @Query() query = { tag: "*", limit: 50, sortKey: "ID", reverse: false }
+  ) {
+    console.log( SortKey[sortKey as keyof typeof SortKey])
+    const myshopifyDomain = req.session[`shopify-connect-${req.shop}`].shop.myshopify_domain;
+    const route = `shopify/api-ext/products/?` + Object.keys(query).map(k => `${k}=${query[k]}`).join('&');
+    const key = JSON.stringify({
+      route,
+      myshopifyDomain
+    });
     try {
-      const products = await this.extProductsService.listScheduled(req.session[`shopify-connect-${req.shop}`], {
-        limit: limit,
-        tag: tag,
-        sortKey: SortKey[sortKey as keyof typeof SortKey],
-        reverse: reverse,
-        after: after
-      });
-      return res.jsonp(products);
+      return res.jsonp(
+        await this.cache.wrap<any>(
+          key,
+          () => this.extProductsService.listScheduled(
+            req.session[`shopify-connect-${req.shop}`], {
+            limit,
+            tag,
+            sortKey: SortKey[sortKey as keyof typeof SortKey],
+            reverse,
+            after
+          }),
+        { ttl: 300 }
+      ));
     } catch (error) {
       this.logger.error(error);
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR).jsonp({
@@ -63,17 +91,24 @@ export class ExtProductsController {
   @UseGuards(ShopifyApiGuard, RolesGuard)
   @Roles() // Also allowed from shop frontend
   @Get('preview')
-  @CacheTTL(1800)
+  // @CacheTTL(1800)
   async getPreview(
     @Req() req: IUserRequest,
     @Res() res: Response,
     @Query("id") id,
   ) {
+    const myshopifyDomain = req.session[`shopify-connect-${req.shop}`].shop.myshopify_domain;
+    const route = `shopify/api-ext/products/?id=${id}`;
+    const key = JSON.stringify({
+      route,
+      myshopifyDomain
+    });
     try {
-      const products = await this.extProductsService.getPreview(req.session[`shopify-connect-${req.shop}`], {
-        id
-      });
-      return res.jsonp(products);
+      return res.jsonp(await this.cache.wrap<any>(
+        key,
+        () => this.extProductsService.getPreview(req.session[`shopify-connect-${req.shop}`], {id}),
+        { ttl: 1800 }
+      ));
     } catch (error) {
       this.logger.error(error);
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR).jsonp({
