d80cc4b70f5085f4a6a4e3b9d934392a08861800
Moritz Raguschat <moritz@jumplink.me>
2021-01-13 00:48:49 +0700

    Revert "[AuthController] add '.myshopify.com' to shop parameter if missing"
    This reverts commit 6fbff8a5fd12698743bf9ca76c376a4a44f1a972.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -63,12 +63,8 @@ export class ShopifyAuthController {
     @Session() session: IUserSession,
   ) {
     shop =  shop || shopParam || shopBody || this.shopifyAuthService.getShopFromRequest(req);
-    if (typeof shop !== 'string' || shop === '') {
+    if (typeof shop !== 'string') {
       return res.send('shop was not a string, e.g. /auth/shopify?shop=your-shop-name');
-    }
-
-    if (!shop.endsWith('.myshopify.com')) {
-      shop = shop + '.myshopify.com';
     }
 
     // Logout if the user is logged in another shop
@@ -109,12 +105,8 @@ export class ShopifyAuthController {
     @Session() session: IUserSession,
   ) {
     shop = shop || shopParam || shopBody || req.headers.shop as string || req.headers['x-shopify-shop-domain'] as string || req.headers['X-Shopify-Shop-Domain'] as string;
-    if (typeof shop !== 'string' || shop === '') {
+    if (typeof shop !== 'string') {
       throw new HttpException('shop was not a string, e.g. /auth/shopify?shop=your-shop-name', HttpStatus.BAD_REQUEST);
-    }
-
-    if (!shop.endsWith('.myshopify.com')) {
-      shop = shop + '.myshopify.com';
     }
 
     session.currentShop = shop;
