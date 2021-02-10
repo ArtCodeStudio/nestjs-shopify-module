551f8e9736ee8dc642fb5fda8a6f4ac9485e2346
Pascal Garber <pascal@artandcode.studio>
2021-02-10 14:46:30 +0000

    Upgrade to nest 8 alpha

--- src/socket/session-io.adapter.ts
+++ src/socket/session-io.adapter.ts
@@ -1,21 +1,11 @@
-// import { NestApplication } from '@nestjs/core';
 import { INestApplicationContext } from '@nestjs/common';
-import { isFunction, isNil } from '@nestjs/common/utils/shared.utils';
-import {
-  AbstractWsAdapter,
-  MessageMappingProperties,
-} from '@nestjs/websockets';
-import { DISCONNECT_EVENT } from '@nestjs/websockets/constants';
-import { fromEvent, Observable } from 'rxjs';
-import { filter, first, map, mergeMap, share, takeUntil } from 'rxjs/operators';
-import { Socket, Server } from 'socket.io';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { Socket } from 'socket.io';
 import { NextFunction, RequestHandler } from 'express';
-import { CustomOrigin } from '@nestjs/common/interfaces/external/cors-options.interface';
 import * as sharedsession from 'express-socket.io-session';
 import { DebugService } from '../debug.service';
 
-// TODO: Using this until socket.io v3 is part of Nest.js, see: https://github.com/nestjs/nest/issues/5676
-export class SessionIoAdapter extends AbstractWsAdapter {
+export class SessionIoAdapter extends IoAdapter {
 
   protected logger = new DebugService(`shopify:${this.constructor.name}`);
   protected socketSessionMiddleware: (
@@ -24,15 +14,9 @@ export class SessionIoAdapter extends AbstractWsAdapter {
   ) => void;
   constructor(
     session: RequestHandler,
-    appOrHttpServer?: INestApplicationContext | any,
-    private corsOrigin?:
-      | boolean
-      | string
-      | RegExp
-      | (string | RegExp)[]
-      | CustomOrigin,
+    appOrHttpServer?: INestApplicationContext | any
   ) {
-    super(appOrHttpServer.getHttpServer());
+    super(appOrHttpServer);
     /**
      * Make session available on socket.io client socket object
      * @see https://github.com/oskosk/express-socket.io-session
@@ -42,95 +26,9 @@ export class SessionIoAdapter extends AbstractWsAdapter {
     });
   }
 
-  public create(
-    port: number,
-    options?: any & { namespace?: string; server?: any },
-  ): any {
-    if (!options) {
-      return this.createIOServer(port);
-    }
-    const { namespace, server, ...opt } = options;
-    if (server && isFunction(server.of)) {
-      if (server.of.namespace) {
-        return this.createIOServer(port, opt).of(namespace);
-      } else {
-        return this.createIOServer(port, opt);
-      }
-    } else {
-      if (namespace) {
-        return this.createIOServer(port, opt).of(namespace);
-      } else {
-        return this.createIOServer(port, opt);
-      }
-    }
-  }
-
   public createIOServer(port: number, options?: any): any {
-    let server;
-    if (this.httpServer && port === 0) {
-      server = new Server(this.httpServer, {
-        cors: {
-          origin: this.corsOrigin,
-          methods: ['GET', 'POST'],
-          credentials: true,
-        },
-        cookie: {
-          httpOnly: true,
-          path: '/',
-        },
-        // Allow 1MB of data per request.
-        maxHttpBufferSize: 1e6,
-      });
-    } else {
-      server = new Server(port, options);
-    }
+    const server = super.createIOServer(port, options);
     server.use(this.socketSessionMiddleware);
     return server;
-  }
-
-  public bindMessageHandlers(
-    client: any,
-    handlers: MessageMappingProperties[],
-    transform: (data: any) => Observable<any>,
-  ) {
-    const disconnect$ = fromEvent(client, DISCONNECT_EVENT).pipe(
-      share(),
-      first(),
-    );
-
-    handlers.forEach(({ message, callback }) => {
-      const source$ = fromEvent(client, message).pipe(
-        mergeMap((payload: any) => {
-          const { data, ack } = this.mapPayload(payload);
-          return transform(callback(data, ack)).pipe(
-            filter((response: any) => !isNil(response)),
-            map((response: any) => [response, ack]),
-          );
-        }),
-        takeUntil(disconnect$),
-      );
-      source$.subscribe(([response, ack]) => {
-        if (response.event) {
-          return client.emit(response.event, response.data);
-        }
-        isFunction(ack) && ack(response);
-      });
-    });
-  }
-
-  public mapPayload(payload: any): { data: any; ack?: () => any } {
-    if (!Array.isArray(payload)) {
-      return { data: payload };
-    }
-    const lastElement = payload[payload.length - 1];
-    const isAck = isFunction(lastElement);
-    if (isAck) {
-      const size = payload.length - 1;
-      return {
-        data: size === 1 ? payload[0] : payload.slice(0, size),
-        ack: lastElement,
-      };
-    }
-    return { data: payload };
   }
 }
